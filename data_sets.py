6859ebf27f74866fec4f7125a92db418f8f50617
Yahir-Ponce <yahir.ponce@softtek.com>
2025-09-29 14:33:15 -0600

    [REPLACE] datasets -> data_sets to avoid conflicts

--- data_sets.py
+++ data_sets.py
@@ -2,10 +2,13 @@ from datasets import load_dataset
 import json
 
 # Cargar dataset
-sbf = load_dataset('SWE-bench/SWE-bench_Lite', split="test")
-
+# dev = load_dataset('SWE-bench/SWE-bench', split='dev')
+test = load_dataset('SWE-bench/SWE-bench', split='test')
+# train = load_dataset('SWE-bench/SWE-bench', split='train')
+# tot_len = len(dev) + len(test) + len(train)
+print(f"Dataset cargado con {len(test)} entradas.")
 # Convertir a lista de diccionarios
-data = [item for item in sbf]
+data = [item for item in test]
 
 # Guardar en un archivo JSON
 with open("swe_bench_lite_test.json", "w", encoding="utf-8") as f:
